39ee7ea1b26e069f5f41fa2c0fb4d143daa206f7
Jeffrey Wang <jeffrey868@gmail.com>
2020-01-31 17:08:21 -0500

    Add mat analysis

--- mat_base_analysis.py
+++ mat_base_analysis.py
@@ -13,23 +13,26 @@ mu_s = 0.62 # Wood on Concrete Coefficient of Static Friction
 rho_plexi = 1180 # Density of plexiglass (kg/m^3)
 
 # User changed variables
-h = 0.6 # Height of board (M)
-w = 0.6 # Width of board (M)
-hb = 0.15 # Height of base (M)
-wb = 0.6 # Width of base (M)
-t = 0.0191 # Thickness of board (M)
+h = 0.6096 # Height of board (M)
+w = 0.6096 # Width of board (M)
+hb = 0.02 # Height of base (M)
+wb = 1 # Width of base (M)
+t = 0.013716 # Thickness of board (M)
 db = 0.1 # Distance between bottom of board and bottom of base (M)
+kid_mass = 17.5 # Mass of a 5 year old (kg) https://wicworks.fns.usda.gov/sites/default/files/media/document/CDC%20WIC%20Growth%20Charts%202-5yrsBoys.pdf
 
 # Calculated variables
 mboard = (h * w * t) * rho_plexi # mass of board (kg)
 Gboard = mboard * g # gravitational force of board (N)
+Gkid = kid_mass * g # gravitational force of kid on mat (N)
 
 # Base mass calculations
-mbase = ((Fpush * h) + (Fpush * hb) - (Gboard * 0.5 * wb)) / (0.5 * wb * g)
+mbase = ((Fpush * h) + (Fpush * hb) - (Gboard * 0.5 * wb) - (Gkid * 0.5 * wb)) / (0.5 * wb * g)
 
 # Slipping calculation
-if (Fpush > mu_s * (Gboard + mbase * g)):
-	print("The board will slip. Revisit design.")
+if (Fpush > mu_s * (Gboard + mbase * g + Gkid)):
+	print(f"{Fpush} > {mu_s * (Gboard + mbase * g + Gkid)}")
+	print(f"The board will slip. Base needs to be {Fpush - mu_s * (Gboard + mbase * g + Gkid)}kg heavier.")
 else:
 	print(f"The mass of the base needs to be at least {mbase}kg to prevent tipping.")
 
